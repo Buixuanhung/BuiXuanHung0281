aad28848848a3fb877241930aab1773d83ed6a5c
Buixuanhung <buixuanhung16042001@gmail.com>
2023-04-24 15:21:19 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BuiXuanHung0281.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
